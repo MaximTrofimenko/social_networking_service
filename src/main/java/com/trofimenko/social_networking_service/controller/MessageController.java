1dcc80b31c1c0bf9662e9dde9d2b98022cbdab4e
MaximTrofimenko <oklickk@yandex.ru>
2020-04-24 14:06:36 +0300

    добавил post метод

--- src/main/java/com/trofimenko/social_networking_service/controller/MessageController.java
+++ src/main/java/com/trofimenko/social_networking_service/controller/MessageController.java
@@ -1,10 +1,7 @@
 package com.trofimenko.social_networking_service.controller;
 
 import com.trofimenko.social_networking_service.exeptions.NotFoundException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,7 +11,9 @@ import java.util.Map;
 @RestController
 @RequestMapping("message")
 public class MessageController {
-    List<Map<String,String>> messages = new ArrayList<Map<String,String>>(){{
+    private int count = 4;
+
+    private List<Map<String,String>> messages = new ArrayList<Map<String,String>>(){{
         add(new HashMap<String, String>(){{put("id","1");put("text","First message");}});
         add(new HashMap<String, String>(){{put("id","2");put("text","Second message");}});
         add(new HashMap<String, String>(){{put("id","3");put("text","Third message");}});
@@ -32,4 +31,13 @@ public class MessageController {
                 .findFirst()
                 .orElseThrow(NotFoundException::new);
     }
+
+    @PostMapping
+    public Map<String,String> create(@RequestBody Map<String,String> message){
+        message.put("id",String.valueOf(count));
+        messages.add(message);
+        return message;
+
+    }
+
 }
