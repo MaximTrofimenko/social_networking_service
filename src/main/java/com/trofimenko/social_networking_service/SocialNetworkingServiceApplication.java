bb6599839a04b9f007d06ed33c179671895e9f38
MaximTrofimenko <oklickk@yandex.ru>
2020-04-24 07:40:10 +0300

    Initial commit

---
+++ src/main/java/com/trofimenko/social_networking_service/SocialNetworkingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trofimenko.social_networking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trofimenko/social_networking_service/SocialNetworkingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trofimenko.social_networking_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
