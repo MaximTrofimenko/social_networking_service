96a72f1ccaa3e5462283210abc2938c77ede2adc
MaximTrofimenko <oklickk@yandex.ru>
2020-04-24 13:57:29 +0300

    добавил get метод для поиска одного собщения по id

--- src/main/java/com/trofimenko/social_networking_service/controller/MessageController.java
+++ src/main/java/com/trofimenko/social_networking_service/controller/MessageController.java
@@ -1,6 +1,8 @@
 package com.trofimenko.social_networking_service.controller;
 
+import com.trofimenko.social_networking_service.exeptions.NotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,11 +16,20 @@ import java.util.Map;
 public class MessageController {
     List<Map<String,String>> messages = new ArrayList<Map<String,String>>(){{
         add(new HashMap<String, String>(){{put("id","1");put("text","First message");}});
-        add(new HashMap<String, String>(){{put("id","2");put("text","Secong message");}});
+        add(new HashMap<String, String>(){{put("id","2");put("text","Second message");}});
         add(new HashMap<String, String>(){{put("id","3");put("text","Third message");}});
     }};
+
     @GetMapping
     public List<Map<String,String>> list(){
         return messages;
+    }
+
+    @GetMapping("{id}")
+    public Map<String,String> getOne(@PathVariable String id){
+        return messages.stream()
+                .filter(messages -> messages.get("id").equals(id))
+                .findFirst()
+                .orElseThrow(NotFoundException::new);
     }
 }
