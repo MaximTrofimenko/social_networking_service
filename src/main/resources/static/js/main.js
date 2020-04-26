2713b1cab24815d220dc2c1168518e6bb6e52246
MaximTrofimenko <oklickk@yandex.ru>
2020-04-26 16:29:17 +0300

    использование компонента Vue.js

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,6 +1,16 @@
+Vue.component('messages-list', {
+    props: ['messages'],
+    template: '<div><div v-for="message in messages">{{message.text}}</div></div>'
+});
+
 var app = new Vue({
     el: '#app',
+    template: '<messages-list :messages="messages"/>',
     data: {
-        message: 'Привет, Vue!'
+        messages: [
+            {id:'123',text:'Wow'},
+            {id:'23',text:'More'},
+            {id:'3',text:'Another'},
+        ]
     }
 });
